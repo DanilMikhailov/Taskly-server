51a346ab295477bcba86197ee6d08e5b12fadca6
DanilMikhailov <78970984+DanilMikhailov@users.noreply.github.com>
2024-11-01 21:24:29 +0500

    Fix env and server

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use(cookieParser()); 
 app.use(fileUpload()); 
 
-app.use(cors({ 
-    origin: process.env.CLIENT_URL, 
-    credentials: true, 
-    }) 
-    );
+app.use(cors({
+    origin: process.env.PORT,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Укажите методы, которые вы хотите разрешить
+    credentials: true, // Если нужно отправлять куки
+}));
 
 // Middleware для обработки JSON и cookies
 app.use(express.json());
